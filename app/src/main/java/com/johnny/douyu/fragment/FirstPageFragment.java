92cfd9efe9d3131ad15481db2566188430776d29
qianglifei <234>
2016-08-26 19:56:27 +0800

    Initial commit

---
+++ app/src/main/java/com/johnny/douyu/fragment/FirstPageFragment.java
@@ -0,0 +1,10 @@
+package com.johnny.douyu.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2016/8/26.
+ */
+public class FirstPageFragment extends Fragment {
+
+}
